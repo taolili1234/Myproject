0ce0a04bf6285847ca301077646ab65e2161614f
irisa <82490630+taolili1234@users.noreply.github.com>
2022-10-21 16:11:43 +0900

    10-21

--- Assets/Script/CarController.cs
+++ Assets/Script/CarController.cs
@@ -7,51 +7,81 @@ public class CarController: MonoBehaviour
     public List<WheelCollider> wheels;
     public List<Transform> wheelModel;
     public Transform center;
-    public float LeftShiftSpeed;
+    //public float LeftShiftSpeed;
+    public float c = 0;
 
     private void Awake()
     {
-        Application.targetFrameRate = 60;
+        //Application.targetFrameRate = 60;
     }
     void Start()
     {
         GetComponent<Rigidbody>().centerOfMass = center.localPosition;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
-   
-    private void Update()
+
+     void FixedUpdate()
     {
-        float s = 0;
+        float s = 2500;
         float a = 0;
         float b = 0;
+        if (!Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                this.GetComponent<Rigidbody>().drag = 0f;
+                for (int i = 0; i <= 1000; i +=10)
+                {
+                    s = -i;
+                }
+            }
+            else if(Input.GetKeyUp(KeyCode.S))
+            {
+                this.GetComponent<Rigidbody>().drag = 1f;
+            }
+
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                a = -45;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                a = 45;
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                
+                b = 1000;
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                for (int  i = 0; i<= s; i += 2)
+                {
+                    s = i;
+                }
+            }
+            else if(Input.GetKeyUp(KeyCode.LeftShift)&& Input.GetKey(KeyCode.W))
+            {
+                for (int i = 0; i <= 2500; i += 2)
+                {
+                    for(int l=1;l<=10;l++)
+                    {
+                        s = i * l;
+                    }
+                }
+            }
+        
+        }
+        else
+        {
+            this.GetComponent<Rigidbody>().drag = 1f;
+        }
        
-        if (Input.GetKey(KeyCode.S))
-        {
-            s = -1000;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            a = -25;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            a = 25;
-        }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            b=1000;
-        }
-
-        if (Input.GetKey(KeyCode.LeftShift)&& Input.GetKey(KeyCode.W))
-        {
-             s= 2000* LeftShiftSpeed;
-            Debug.Log("sss");
-        }
-        else if (Input.GetKey(KeyCode.W))
-        {
-
-            s = 2000;
-        }
 
         for (int i=0;i<wheels.Count;i++)
         {
@@ -63,8 +93,8 @@ public class CarController: MonoBehaviour
             wheelModel[i].position = pos;
             wheelModel[i].rotation = rot;
         }
-        wheels[0].steerAngle = Mathf.Lerp(wheels[0].steerAngle,a,0.1f);
-        wheels[1].steerAngle = Mathf.Lerp(wheels[1].steerAngle, a, 0.1f);
-        
+        wheels[0].steerAngle = Mathf.Lerp(wheels[0].steerAngle,a,c);
+        wheels[1].steerAngle = Mathf.Lerp(wheels[1].steerAngle, a,c);
     }
+    
 }

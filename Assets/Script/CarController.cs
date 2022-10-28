761265d700a37a7110a744e700a0d8b04160fa7b
irisa <82490630+taolili1234@users.noreply.github.com>
2022-10-28 16:10:27 +0900

    10-28

--- Assets/Script/CarController.cs
+++ Assets/Script/CarController.cs
@@ -27,7 +27,6 @@ public class CarController : MonoBehaviour
     float _brakeTorque;
 
 
-
     private void Awake()
     {
         Application.targetFrameRate = 120;
@@ -42,26 +41,57 @@ public class CarController : MonoBehaviour
 
     void Update()
     {
+        WheelFrictionCurve forwardFriction = new WheelFrictionCurve();
+        forwardFriction.extremumSlip = 0.4f;
+        forwardFriction.extremumValue = 1;
+        forwardFriction.asymptoteSlip = 0.5f;
+        forwardFriction.asymptoteValue = 0.8f;
+        forwardFriction.stiffness = 6f;
+
+        WheelFrictionCurve sidewaysFriction = new WheelFrictionCurve();
+        sidewaysFriction.extremumSlip = 0.2f;
+        sidewaysFriction.extremumValue = 1f;
+        sidewaysFriction.asymptoteSlip = 0.5f;
+        sidewaysFriction.asymptoteValue = 0.8f;
+        sidewaysFriction.stiffness = 2f;
+
         for (int i = 0; i < wheels.Count; i++) 
         {
             wheelModel[i].Rotate(wheels[i].rpm / 60 * 360 * Time.deltaTime, 0, 0);
             wheels[i].GetWorldPose(out var pos, out var rot);
             wheelModel[i].position = pos;
             wheelModel[i].rotation = rot;
+            wheels[i].forwardFriction = forwardFriction;
+            wheels[i].sidewaysFriction = sidewaysFriction;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                _brakeTorque = 1000f;
+                forwardFriction.extremumSlip = 2.4f;
+                forwardFriction.extremumValue = 1;
+                forwardFriction.asymptoteSlip = 0.8f;
+                forwardFriction.asymptoteValue = 0.8f;
+                forwardFriction.stiffness = 2f;
+
+                sidewaysFriction.extremumSlip = 2.2f;
+                sidewaysFriction.extremumValue = 1f;
+                sidewaysFriction.asymptoteSlip = 0.5f;
+                sidewaysFriction.asymptoteValue = 0.8f;
+                sidewaysFriction.stiffness = 2f;
+
+                wheels[i].forwardFriction = forwardFriction;
+                wheels[i].sidewaysFriction = sidewaysFriction;
+                Debug.Log("sss");
+
+            }
+            else
+            {
+                _brakeTorque = 0f;
+            }
         }
         wheels[0].brakeTorque = _brakeTorque;
         wheels[1].brakeTorque = _brakeTorque;
-        if (Input.GetKey(KeyCode.Space))
-        {        
-            _brakeTorque = 1000f;
-            Debug.Log("sss");
-        }
-        else
-        {
-            _brakeTorque = 0f;
-        }
-
-
+       
     }
 
     public void FixedUpdate()
@@ -79,8 +109,8 @@ public class CarController : MonoBehaviour
             }
             if (axleInfo.motor)
             { 
-                axleInfo.leftWheel.motorTorque = motor;
-                axleInfo.rightWheel.motorTorque = motor;
+                axleInfo.leftWheel.motorTorque = motor*2;
+                axleInfo.rightWheel.motorTorque = motor*2;
             }
         }
     }
